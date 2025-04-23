25810d20ac2a58f734102b1410e0942b3d4ca4cd
Julia Krznarich <krznari4@msu.edu>
2025-04-23 08:13:59 +0000

    committing report

--- OpenMM Example/openmm_example.py
+++ OpenMM Example/openmm_example.py
@@ -44,25 +44,3 @@ simulation.context.reinitialize(preserveState=True)
 print("Running NPT")
 simulation.step(10000)
 
-import numpy as np
-import matplotlib.pyplot as plt
-data = np.loadtxt("md_log.txt", delimiter=',')
-
-step = data[:,0]
-potential_energy = data[:,1]
-temperature = data[:,2]
-volume = data[:,3]
-
-plt.plot(step, potential_energy)
-plt.xlabel("Step")
-plt.ylabel("Potential energy (kJ/mol)")
-plt.show()
-plt.plot(step, temperature)
-plt.xlabel("Step")
-plt.ylabel("Temperature (K)")
-plt.show()
-plt.plot(step, volume)
-plt.xlabel("Step")
-plt.ylabel("Volume (nm^3)")
-plt.show()
-
